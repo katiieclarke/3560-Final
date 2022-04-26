2f747e8d27fcc4a96835cab392f382f8f7ebfcbd
Katie Clarke <>
2022-04-26 11:26:58 -0400

    Count.cc and MakeFile

--- count.cc
+++ count.cc
@@ -7,23 +7,32 @@ int countLine(string text){
     int count = 0;
     int i = 0;
     while(text[i++] != '\0'){
-        getline(i,text);
+        istream& getline(char* , int size);
         count++;
     }
 
+    cout << count << " Lines" << endl;
     return count;
 }
 
-int countChar(string text){
+int countChar(char *text){
     int count = 0;
     int i = 0;
     while(text[i++] != '\0'){
         count++;
     }
 
+    cout << count << " Characters" << endl;
     return count;
 }
 
-int main(){
+int main(int argc, char** argv){
     
+    for(int i = 0; i < argc; i++){
+        cout << setw(5) << i << setw(8) << countChar(argv[i]) << " " << argv[i] << endl;
+
+        cout << setw(5) << countLine(argv[i]) << endl;
+    }
+
+    return 0;
 }
