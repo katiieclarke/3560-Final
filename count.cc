de10f0394c4cc6f179814e7e30a0f4bd366f5650
Katie Clarke <>
2022-04-26 11:36:00 -0400

    doxygen

--- count.cc
+++ count.cc
@@ -1,8 +1,22 @@
+/**********
+ * @File count.cc
+ * @Author Katie Clarke
+ * @Brief count.cc counts the number of lines and characters
+ * Count.cc
+ * Course: CS 3560
+ * Date: April 26, 2022
+ * **********/
+
 #include <iostream> 
 #include <iomanip> 
 
 using namespace std;
 
+/*********************************************************
+ *Function: int countLine(string text)
+ * Parameters: None
+ * Purpose: to count the number of lines used in arguments
+ *******************************************************/
 int countLine(string text){
     int count = 0;
     int i = 0;
@@ -15,6 +29,11 @@ int countLine(string text){
     return count;
 }
 
+/*********************************************************
+ *Function: int countChar(char *text)
+ * Parameters: None
+ * Purpose: to count the number of characters in an argument
+ *******************************************************/
 int countChar(char *text){
     int count = 0;
     int i = 0;
@@ -26,6 +45,12 @@ int countChar(char *text){
     return count;
 }
 
+/*********************************************************
+ *Function: int main(int argc, char** argv)
+ * Parameters: None
+ * Purpose: to execute functions countChar and countLine
+ * to print out the answers after compiling
+ *******************************************************/
 int main(int argc, char** argv){
     
     for(int i = 0; i < argc; i++){
---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['3560_2dfinal_0',['3560-Final',['../md__r_e_a_d_m_e.html',1,'']]]
+];
